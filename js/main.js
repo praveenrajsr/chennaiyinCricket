8ed07f7121dc7f36a7495d91f363339e01a7cff8
praveenrajsr <aashooshome@gmail.com>
2018-06-09 11:15:47 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,191 @@
+//Tamil Translation
+
+var aLangKeys = new Array();
+aLangKeys['en'] = new Array();
+aLangKeys['tn'] = new Array();
+
+aLangKeys['en']['primary'] = 'Primary Details';
+aLangKeys['en']['special'] = 'Special Ability';
+aLangKeys['en']['payment'] = "Payment";
+aLangKeys['en']['name'] = 'Name';
+aLangKeys['en']['dob'] = 'Date Of Birth';
+aLangKeys['en']['email'] = 'Email Address*';
+aLangKeys['en']['number'] = 'Phone Number*';
+aLangKeys['en']['address'] = 'Your Address*';
+aLangKeys['en']['aadhar'] = 'Aadhar Number*';
+aLangKeys['en']['category'] = 'Category*';
+aLangKeys['en']['employee'] = 'Employee';
+aLangKeys['en']['student'] = 'Student';
+aLangKeys['en']['id'] = 'Id*';
+aLangKeys['en']['company'] = 'Company';
+aLangKeys['en']['college'] = 'College';
+aLangKeys['en']['extended'] = 'Extended Details';
+aLangKeys['en']['parent'] = 'Parent or Guardian Name*';
+aLangKeys['en']['parentNum'] = 'Parent Contact Number*';
+aLangKeys['en']['parentAadhar'] = 'Parent Aadhar Number*';
+aLangKeys['en']['physical']='Physical Details';
+aLangKeys['en']['surgery'] = 'Surgery if any';
+aLangKeys['en']['illness'] = 'Affected illness if any';
+aLangKeys['en']['company'] = 'Company or Institution name *'
+
+aLangKeys['en']['title'] = 'TELL US YOUR SPECIAL ABILITY';
+aLangKeys['en']['primary'] = 'Primary Details';
+aLangKeys['en']['special'] = 'Special Ability';
+aLangKeys['en']['payment'] = 'Payment';
+aLangKeys['en']['batting'] = 'Batting';
+aLangKeys['en']['batPreference']= 'Choose your Batting preference';
+aLangKeys['en']['right']= 'Right';
+aLangKeys['en']['left']= 'Left';
+aLangKeys['en']['bowling']= 'Bowling';
+aLangKeys['en']['hand']= 'Hand';
+aLangKeys['en']['bowlPreference']= 'Choose your Bowling preference';
+aLangKeys['en']['mode']= 'Choose your Bowling Mode*';
+aLangKeys['en']['spin']= 'Spin';
+aLangKeys['en']['pace']= 'Pace';
+aLangKeys['en']['medium']= 'Medium';
+aLangKeys['en']['spinMode']= 'Choose your Spin mode';
+aLangKeys['en']['off']="offside";
+aLangKeys['en']['leg']="legside";
+aLangKeys['en']['pacemode']="Choose your Pace Mode*";
+aLangKeys['en']['in']= 'In';
+aLangKeys['en']['out']= 'Out';
+aLangKeys['en']['mediummode']="Choose your Medium Mode*"
+aLangKeys['en']['specialballs']= 'Special balls if any';
+aLangKeys['en']['fielding']= 'Fielding';
+aLangKeys['en']['goodAt']= 'Good At';
+aLangKeys['en']['slips']= 'Slips';
+aLangKeys['en']['thirdMan']= 'Third Man';
+aLangKeys['en']['gully']= 'Gully ';
+aLangKeys['en']['point']= 'Point ';
+aLangKeys['en']['deepPoint']= 'Deep Point';
+aLangKeys['en']['deepCover']= 'Deep Cover';
+aLangKeys['en']['extraCover']= 'Extra Cover';
+aLangKeys['en']['deepExtra']= 'Deep Extra';
+aLangKeys['en']['minOn']= 'Mid On';
+aLangKeys['en']['midOff']= 'Mid Off';
+aLangKeys['en']['longOn']= 'Long On';
+aLangKeys['en']['longOff']= 'Long OFf';
+aLangKeys['en']['squareLeg']= 'Square Leg';
+aLangKeys['en']['deepSquare']= 'Deep square Leg';
+aLangKeys['en']['fineLeg']= 'Fine leg';
+aLangKeys['en']['midWicket']= 'Mid Wicket';
+aLangKeys['en']['deepMid']= 'Deep mid wicket';
+aLangKeys['en']['fieldingPreferences']= ' Your Fielding Preferences(Select any 3)';
+aLangKeys['en']['wicket']= 'Wicket Keeper';
+aLangKeys['en']['yes']= 'Yes';
+aLangKeys['en']['no']= 'No';
+aLangKeys['en']['play']= 'Lets Play';
+
+//tamil translation
+aLangKeys['tn']['primary'] = 'முதன்மை தகவல்கள்';
+aLangKeys['tn']['special'] = 'சிறப்புத் தகுதி ';
+aLangKeys['tn']['payment'] = "கட்டணம்";
+aLangKeys['tn']['name'] = 'பெயர்*';
+aLangKeys['tn']['dob'] = 'பிறந்த தேதி*';
+aLangKeys['tn']['email'] = 'மின்னஞ்சல்*';
+aLangKeys['tn']['number'] = 'தொலைபேசி எண்*';
+aLangKeys['tn']['address'] = 'தங்களின் முகவரி*';
+aLangKeys['tn']['aadhar'] = 'ஆதார் எண் *';
+aLangKeys['tn']['category'] = 'பகுப்பு*';
+aLangKeys['tn']['employee'] = 'ஊழியர்';
+aLangKeys['tn']['student'] = 'மாணவர்';
+aLangKeys['tn']['id'] = 'அடையாளம்*';
+aLangKeys['tn']['company'] = 'நிறுவனம்';
+aLangKeys['tn']['college'] = 'கல்லூரி';
+aLangKeys['tn']['extended'] = 'விரிவான விவரங்கள்';
+aLangKeys['tn']['parent'] = 'பெற்றோர் அல்லது காப்பாளர் பெயர்*';
+aLangKeys['tn']['parentNum'] = 'பெற்றோர் அல்லது காப்பாளர் தொடர்பு எண்*';
+aLangKeys['tn']['parentAadhar'] = 'பெற்றோர் ஆதார் எண்*';
+aLangKeys['tn']['surgery'] = 'அறுவை சிகிச்சை ஏதாவது இருந்தால்';
+aLangKeys['tn']['illness'] = 'பாதிக்கப்பட்ட நோய் ஏதாவது இருந்தால்';
+aLangKeys['tn']['physical']='உடல் விவரங்கள்';
+aLangKeys['tn']['company'] = 'நிறுவனம் அல்லது கல்வி நிலையம் பெயர்*';
+
+aLangKeys['tn']['title'] = 'உங்கள் சிறப்புத் தகுதியைக் கூறவும்';
+aLangKeys['tn']['primary'] = 'முதன்மை தகவல்கள்';
+aLangKeys['tn']['special'] = 'சிறப்புத் தகுதி ';
+aLangKeys['tn']['payment'] = "கட்டணம்";
+aLangKeys['tn']['batting'] = "மட்டையாளர்";
+aLangKeys['tn']['batPreference']= 'மட்டையாளர் விருப்பம் தேர்வு செய்யுங்கள்*';
+aLangKeys['tn']['right']= 'வலப்பக்கம்';
+aLangKeys['tn']['left']= 'இடப்பக்கம்';
+aLangKeys['tn']['bowling']= 'பந்து வீச்சு ';
+aLangKeys['tn']['hand']= 'பக்கம்';
+aLangKeys['tn']['bowlPreference']= 'உங்கள் பந்து வீச்சு விருப்பம் தேர்வு செய்யுங்கள்*';
+aLangKeys['tn']['mode']= 'பந்து வீச்சு முறை*';
+aLangKeys['tn']['spin']= 'சுழல் பந்து';
+aLangKeys['tn']['pace']= 'வேகப் பந்து';
+aLangKeys['tn']['medium']= 'மித வேகப் பந்து';
+aLangKeys['tn']['spinMode']= 'சுழல் பந்து வீச்சு முறையைத் தேர்வு செய்க*';
+aLangKeys['tn']['off']="ஆஃ ப்சைடு";
+aLangKeys['tn']['leg']="லெக் சைடு";
+aLangKeys['tn']['pacemode']="வேகப் பந்து வீச்சு முறையைத் தேர்வு செய்க*";
+aLangKeys['tn']['in']= 'உள்ளே ';
+aLangKeys['tn']['out']= 'வெளியே';
+aLangKeys['tn']['mediummode']="மித வேகப் பந்து வீச்சு முறையைத் தேர்வு செய்க*"
+aLangKeys['tn']['specialballs']= 'உங்களின் சிறப்புப் பந்துகள் ஏதாவது இருந்தால்';
+aLangKeys['tn']['fielding']= 'கள ஆட்டம்';
+aLangKeys['tn']['goodAt']= 'தாங்கள் தேர்ச்சி பெற்றது ';
+aLangKeys['tn']['slips']= 'ஸ்லிப் ';
+aLangKeys['tn']['thirdMan']= 'தெர்ட் மேன்';
+aLangKeys['tn']['gully']= 'கல்லி ';
+aLangKeys['tn']['point']= 'பாய்ண்ட் ';
+aLangKeys['tn']['deepPoint']= 'டீப் பாய்ண்ட் ';
+aLangKeys['tn']['deepCover']= 'டீப் கவர் ';
+aLangKeys['tn']['extraCover']= 'எக்ஸ்ட்ரா கவர் ';
+aLangKeys['tn']['deepExtra']= 'டீப் எக்ஸ்ட்ரா';
+aLangKeys['tn']['minOn']= 'மிட் ஆன்';
+aLangKeys['tn']['midOff']= 'மிட் ஆஃப் ';
+aLangKeys['tn']['longOn']= 'லாங் ஆன் ';
+aLangKeys['tn']['longOff']= 'லாங் ஆஃப்';
+aLangKeys['tn']['squareLeg']= 'ஸ்கொயர்  லெக் ';
+aLangKeys['tn']['deepSquare']= 'டீப் ஸ்கொயர்  லெக் ';
+aLangKeys['tn']['fineLeg']= 'ஃபைன் லெக் ';
+aLangKeys['tn']['midWicket']= 'ஃபைன் விக்கெட் ';
+aLangKeys['tn']['deepMid']= 'டீப் மிட் விக்கெட்';
+aLangKeys['tn']['fieldingPreferences']= 'உங்கள்  ஃபில்ட்டிங் விருப்பம்(ஏதேனும் மூன்றை தேர்வு செய்யுங்கள் )';
+aLangKeys['tn']['wicket']= 'விக்கெட் கீப்பர்';
+aLangKeys['tn']['yes']= 'ஆம்';
+aLangKeys['tn']['no']= 'இல்லை';
+aLangKeys['tn']['play']= 'விளையாடுவோமா ';
+
+$(document).ready(function(){
+    var scrollTop = 0; 
+    $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop >= 50){
+            $('.full-logo').addClass('small');
+            $('.nav').addClass('new');
+        }else {
+            $('.full-logo').removeClass('small');
+            $('nav').removeClass('new');
+        }
+    });
+    // Tamil Translation
+    $('.lang').change(function(){
+		var lang = $(this).val();
+		$('.tr').each(function(i){
+			$(this).text(aLangKeys[lang][$(this).attr('key')]);
+		});
+    });
+    // age calc
+    $('#dob').change(function(){
+        var birthDate = $('#dob').val();
+        var newage =  calculateage(birthDate);
+        if(newage < 18){
+            alert('Sorry You must be 18 years older');
+            $('.age').html("Sorry You must be 18 years older");
+        }else{
+            $('.age').html("You are"+" "+ newage + " " + "years old");
+        }
+    });
+
+    var calculateage = function(birthDate){
+        var currentyear = new Date().getFullYear();
+        var birthYear = new Date(birthDate).getFullYear();
+        var age = currentyear - birthYear;
+
+        return age;
+    }
+    
+});
